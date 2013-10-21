cdc8d0d40f1dab35e8591da90956a127d9950dd5
Marcus Bermel <mbermel7@gmail.com>
2013-10-20 18:13:29 -0600

    Tweaks to accusation code

--- src/test/GameActionTests.java
+++ src/test/GameActionTests.java
@@ -19,8 +19,8 @@ public class GameActionTests {
 	private clueGame.Card weapon;
 	private clueGame.Card person;
 	private clueGame.Card room;
-	
-	
+
+
 	@Before
 	public void setUp(){
 		// Set up a clue game to run
@@ -40,12 +40,12 @@ public class GameActionTests {
 		room.setType(clueGame.Card.CardType.ROOM);
 		solution.add(room);
 	}
-	
+
 	@Test
-	public void testAccusation(){
+	public void testGoodAccusation(){
 		// Set the clue game's solution to the simple solution constructed in setup.
 		cg.setSolution(solution);
-		
+
 		// Construct an array with the correct weapon, person, and room
 		ArrayList<clueGame.Card> correctAccusation = new ArrayList<clueGame.Card>();
 		correctAccusation.add(weapon);
@@ -53,7 +53,13 @@ public class GameActionTests {
 		correctAccusation.add(room);
 		// Correct solution should yield a true return
 		Assert.assertTrue(cg.checkAccusation(correctAccusation));
-		
+
+
+	}
+
+	@Test
+	public void testAccusationAllWrong(){
+		cg.setSolution(solution);
 		// Create incorrect weapon, person and room
 		clueGame.Card weapon2 = new clueGame.Card();
 		clueGame.Card person2 = new clueGame.Card();
@@ -70,10 +76,65 @@ public class GameActionTests {
 		falseAccusation.add(person2);
 		falseAccusation.add(room2);
 		// Accusation is incorrect, therefore this should yield false;
-		Assert.assertFalse(cg.checkAccusation(falseAccusation));
-		
-		
-		
+		Assert.assertFalse(cg.checkAccusation(falseAccusation));		
+
+	}
+	
+	@Test
+	public void testAccusationBadRoom(){
+		// Set the clue game's solution to the simple solution constructed in setup.
+		cg.setSolution(solution);
+
+		// Construct an array with the correct weapon, person, and room
+		ArrayList<clueGame.Card> correctAccusation = new ArrayList<clueGame.Card>();
+		clueGame.Card room2 = new clueGame.Card();
+		room2.setName("Bowling");
+		room2.setType(clueGame.Card.CardType.ROOM);
+		correctAccusation.add(weapon);
+		correctAccusation.add(person);
+		correctAccusation.add(room2);
+		// Correct solution should yield a true return
+		Assert.assertFalse(cg.checkAccusation(correctAccusation));
+
+
+	}
+	
+	@Test
+	public void testAccusationBadWeapon(){
+		// Set the clue game's solution to the simple solution constructed in setup.
+		cg.setSolution(solution);
+
+		// Construct an array with the correct weapon, person, and room
+		ArrayList<clueGame.Card> correctAccusation = new ArrayList<clueGame.Card>();
+		clueGame.Card weapon2 = new clueGame.Card();
+		weapon2.setName("Wrong");
+		weapon2.setType(clueGame.Card.CardType.WEAPON);
+		correctAccusation.add(weapon2);
+		correctAccusation.add(person);
+		correctAccusation.add(room);
+		// Correct solution should yield a true return
+		Assert.assertFalse(cg.checkAccusation(correctAccusation));
+
+
+	}
+	
+	@Test
+	public void testAccusationBadPerson(){
+		// Set the clue game's solution to the simple solution constructed in setup.
+		cg.setSolution(solution);
+
+		// Construct an array with the correct weapon, person, and room
+		ArrayList<clueGame.Card> correctAccusation = new ArrayList<clueGame.Card>();
+		clueGame.Card person2 = new clueGame.Card();
+		person2.setName("Wrong");
+		person2.setType(clueGame.Card.CardType.PERSON);
+		correctAccusation.add(weapon);
+		correctAccusation.add(person2);
+		correctAccusation.add(room);
+		// Correct solution should yield a true return
+		Assert.assertFalse(cg.checkAccusation(correctAccusation));
+
+
 	}
 
 }
