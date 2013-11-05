aeddc9d98e9048a63fbcf975489547482b3466da
rs <rsinner@mines.edu>
2013-11-04 19:07:50 -0700

    Finshed what was needed for turning in Clue Game

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,28 +1,31 @@
 package clueGame;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ComputerPlayer extends Player {
 	
 	public BoardCell pickLocation(Set<BoardCell> targets) {
+		Set<BoardCell> t = new HashSet<BoardCell>();
+		t.addAll(targets);
 		for(BoardCell bc : targets){
 			if (bc.isDoorway()) {
 				// If it's a room cell, check to see if it was most recently visited
 				RoomCell rc = (RoomCell) bc;
-				if(rc.getInitial() == getPreviousRoom()){
-					continue;
-				} else {
+				if(rc.getInitial() != getPreviousRoom()){
 					setPreviousRoom(rc.getInitial());
 					return bc;
+				} else if (rc.getInitial() == getPreviousRoom()) {
+					t.remove(rc);
 				}
 			}
 		}
 
 		// Either there were no rooms or the room was previously visited
 		// Generate a random number  to choose the cell to pick.
-		int randomRoom = generateRandomNumber(targets.size());
-		Object[] targArray = targets.toArray();
+		int randomRoom = generateRandomNumber(t.size());
+		Object[] targArray = t.toArray();
 		return (BoardCell) targArray[randomRoom];
 		//return null;
 	}
